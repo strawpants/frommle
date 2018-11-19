7e93bc7fddc88935cb889c1bcd199b87e0acf1c1
Roelof Rietbroek (@grace) <roelof@geod.uni-bonn.de>
2018-11-19 15:43:36 +0100

    add simple time function

--- python/frommle/core/time.py
+++ python/frommle/core/time.py
@@ -1,2 +1,5 @@
+from datetime import datetime
 def decyear2datetime(decyr):
-    return "blah"
+    year=int(decyr)
+    jan1=datetime(year,1,1)
+    return jan1+(decyr-year)*(datetime(year+1,1,1)-jan1)
