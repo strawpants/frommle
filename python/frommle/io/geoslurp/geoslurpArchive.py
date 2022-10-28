bb510d4a32ec9d7ed7b27e1c50638dbb3846cb59
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-28 14:51:09 +0000

    Adding tarfile member sanitization to extractall()

--- python/frommle/io/geoslurp/geoslurpArchive.py
+++ python/frommle/io/geoslurp/geoslurpArchive.py
@@ -184,7 +184,26 @@ class GeoslurpArchive(Group):
             if os.path.exists(arfile):
                 if not os.path.exists(cache):
                     with tarfile.open(arfile,"r:gz") as tid:
-                        tid.extractall(cache)
+                        def is_within_directory(directory, target):
+                            
+                            abs_directory = os.path.abspath(directory)
+                            abs_target = os.path.abspath(target)
+                        
+                            prefix = os.path.commonprefix([abs_directory, abs_target])
+                            
+                            return prefix == abs_directory
+                        
+                        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                        
+                            for member in tar.getmembers():
+                                member_path = os.path.join(path, member.name)
+                                if not is_within_directory(path, member_path):
+                                    raise Exception("Attempted Path Traversal in Tar File")
+                        
+                            tar.extractall(path, members, numeric_owner=numeric_owner) 
+                            
+                        
+                        safe_extract(tid, cache)
         else:
             raise RuntimeError("Archive is not an existing sqlite database")
 
